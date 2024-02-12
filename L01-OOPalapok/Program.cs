ab3d31e6fdab4909f56e6eca93193caf685efb85
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2024-02-12 15:58:24 +0100

    Add project files.

---
+++ L01-OOPalapok/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L01_OOPalapok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SzFA-Labor_2024_tavasz/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SzFA_Labor_2024_tavasz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
